9145fe4010620b1b617bc508ad81ee75026414a8
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-05-20 18:32:23 -0300

    Subindo código novo

--- Api-Semana1/academia/src/main/java/com/residencia/academia/controller/InstrutorController.java
+++ Api-Semana1/academia/src/main/java/com/residencia/academia/controller/InstrutorController.java
@@ -1,11 +1,15 @@
 package com.residencia.academia.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.residencia.academia.entity.Instrutor;
@@ -16,16 +20,36 @@ import com.residencia.academia.service.InstrutorService;
 public class InstrutorController {
 	@Autowired
 	InstrutorService instrutorService;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Instrutor>> findAllInstrutor(){
+	public ResponseEntity<List<Instrutor>> findAllInstrutor() {
 		List<Instrutor> instrutorList = instrutorService.listarTodos();
 		return new ResponseEntity<>(instrutorList, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Instrutor> findInstrutorById(Integer id) {
 		Instrutor instrutor = instrutorService.listarUm(id);
 		return new ResponseEntity<>(instrutor, HttpStatus.OK);
 	}
+
+	@PostMapping
+	public ResponseEntity<Instrutor> saveInstrutor(@RequestBody Instrutor instrutor) {
+		Instrutor novoInstrutor = instrutorService.saveInstrutor(instrutor);
+		return new ResponseEntity<>(novoInstrutor, HttpStatus.CREATED);
+
+	}
+
+	@PutMapping
+	public ResponseEntity<Instrutor> updateInstrutor(@RequestBody Instrutor instrutor) {
+		Instrutor novoInstrutor = instrutorService.updateInstrutor(instrutor);
+		return new ResponseEntity<>(novoInstrutor, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteInstrutor(@PathVariable Integer id) {
+		instrutorService.deleteInstrutor(id);
+		return new ResponseEntity<>("", HttpStatus.OK);
+	}
+
 }
