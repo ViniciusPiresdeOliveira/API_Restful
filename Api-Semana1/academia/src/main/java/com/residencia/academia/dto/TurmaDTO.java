10654848fe613a96c1b75cd354357d0106f8356a
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-05-25 19:56:52 -0300

    Trabalho finalizado

--- Api-Semana1/academia/src/main/java/com/residencia/academia/dto/TurmaDTO.java
+++ Api-Semana1/academia/src/main/java/com/residencia/academia/dto/TurmaDTO.java
@@ -12,73 +12,51 @@ public class TurmaDTO {
 	private Integer duracaoTurma;
 	private Date dataInicio;
 	private Date dataFim;
-	private Instrutor instrutor;
-	private Atividade atividade;
+	private InstrutorDTO instrutorDTO;
+	private AtividadeDTO atividadeDTO;
 	
-
 	public Integer getIdTurma() {
 		return idTurma;
 	}
-
 	public void setIdTurma(Integer idTurma) {
 		this.idTurma = idTurma;
 	}
-
 	public Date getHorarioTurma() {
 		return horarioTurma;
 	}
-
 	public void setHorarioTurma(Date horarioTurma) {
 		this.horarioTurma = horarioTurma;
 	}
-
 	public Integer getDuracaoTurma() {
 		return duracaoTurma;
 	}
-
 	public void setDuracaoTurma(Integer duracaoTurma) {
 		this.duracaoTurma = duracaoTurma;
 	}
-
 	public Date getDataInicio() {
 		return dataInicio;
 	}
-
 	public void setDataInicio(Date dataInicio) {
 		this.dataInicio = dataInicio;
 	}
-
 	public Date getDataFim() {
 		return dataFim;
 	}
-
 	public void setDataFim(Date dataFim) {
 		this.dataFim = dataFim;
 	}
+	public InstrutorDTO getInstrutorDTO() {
+		return instrutorDTO;
+	}
+	public void setInstrutorDTO(InstrutorDTO instrutorDTO) {
+		this.instrutorDTO = instrutorDTO;
+	}
+	public AtividadeDTO getAtividadeDTO() {
+		return atividadeDTO;
+	}
+	public void setAtividadeDTO(AtividadeDTO atividadeDTO) {
+		this.atividadeDTO = atividadeDTO;
+	}
 	
-	
-
-	public Instrutor getInstrutor() {
-		return instrutor;
-	}
-
-	public void setInstrutor(Instrutor instrutor) {
-		this.instrutor = instrutor;
-	}
-
-	public Atividade getAtividade() {
-		return atividade;
-	}
-
-	public void setAtividade(Atividade atividade) {
-		this.atividade = atividade;
-	}
-
-	@Override
-	public String toString() {
-		return "TurmaDTO [idTurma=" + idTurma + ", horarioTurma=" + horarioTurma + ", duracaoTurma=" + duracaoTurma
-				+ ", dataInicio=" + dataInicio + ", dataFim=" + dataFim + ", instrutor=" + instrutor + ", atividade="
-				+ atividade + "]";
-	}
 	
 }
--- Api-Semana1/academia/src/main/java/com/residencia/academia/service/TurmaService.java
+++ Api-Semana1/academia/src/main/java/com/residencia/academia/service/TurmaService.java
@@ -1,30 +1,36 @@
 package com.residencia.academia.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.residencia.academia.dto.AtividadeDTO;
+import com.residencia.academia.dto.InstrutorDTO;
 import com.residencia.academia.dto.TurmaDTO;
-import com.residencia.academia.dto.TurmaDTO;
-import com.residencia.academia.entity.Turma;
+import com.residencia.academia.entity.Atividade;
+import com.residencia.academia.entity.Instrutor;
 import com.residencia.academia.entity.Turma;
 import com.residencia.academia.repository.TurmaRepository;
 
 @Service
 public class TurmaService {
+
 	@Autowired
 	TurmaRepository turmaRepository;
-	
-	public List<Turma> listarTodos(){
+	@Autowired
+	InstrutorService instrutorService;
+	@Autowired
+	AtividadeService atividadeService;
+
+	public List<Turma> listarTodos() {
 		return turmaRepository.findAll();
 	}
-	
+
 	public Turma listarUma(Integer id) {
-			return turmaRepository.findById(id).isPresent() ? turmaRepository.findById(id).get() : null;	
+		return turmaRepository.findById(id).isPresent() ? turmaRepository.findById(id).get() : null;
 	}
-	
+
 	public TurmaDTO listarUmDTO(Integer id) {
 		Turma turma = turmaRepository.findById(id).isPresent() ? turmaRepository.findById(id).get() : null;
 		TurmaDTO turmaDTO = new TurmaDTO();
@@ -33,11 +39,11 @@ public class TurmaService {
 		}
 		return turmaDTO;
 	}
-	
+
 	public Turma saveTurma(Turma turma) {
 		return turmaRepository.save(turma);
 	}
-	
+
 	public TurmaDTO saveTurmaDTO(TurmaDTO turmaDTO) {
 		Turma turma = converterDTOParaEntidade(turmaDTO);
 		Turma novoTurma = turmaRepository.save(turma);
@@ -45,15 +51,14 @@ public class TurmaService {
 		return converterEntidadeParaDTO(novoTurma);
 	}
 
-	
 	public Turma updateTurma(Turma turma) {
 		return turmaRepository.save(turma);
 	}
-	
+
 	public void deleteTurma(Integer id) {
 		turmaRepository.deleteById(id);
 	}
-	
+
 	private Turma converterDTOParaEntidade(TurmaDTO turmaDTO) {
 		Turma turma = new Turma();
 		turma.setDataFim(turmaDTO.getDataFim());
@@ -61,30 +66,33 @@ public class TurmaService {
 		turma.setDuracaoTurma(turmaDTO.getDuracaoTurma());
 		turma.setHorarioTurma(turmaDTO.getHorarioTurma());
 		turma.setIdTurma(turmaDTO.getIdTurma());
-		turma.setInstrutor(turmaDTO.getInstrutor());
-		turma.setAtividade(turmaDTO.getAtividade());
+		Instrutor instrutor = instrutorService.listarUm(turmaDTO.getInstrutorDTO().getIdInstrutor());
+		turma.setInstrutor(instrutor);
+		Atividade atividade = atividadeService.listarUm(turmaDTO.getInstrutorDTO().getIdInstrutor());
+		turma.setAtividade(atividade);
+
 		return turma;
 	}
 
 	private TurmaDTO converterEntidadeParaDTO(Turma turma) {
 		TurmaDTO turmaDTO = new TurmaDTO();
-				turmaDTO.setDataFim(turma.getDataFim());
-				turmaDTO.setDataInicio(turma.getDataInicio());
-				turmaDTO.setDuracaoTurma(turma.getDuracaoTurma());
-				turmaDTO.setHorarioTurma(turma.getHorarioTurma());
-				turmaDTO.setIdTurma(turma.getIdTurma());
-				turmaDTO.setInstrutor(turma.getInstrutor());
-				turmaDTO.setAtividade(turma.getAtividade());
-				return turmaDTO;
-	
+		turmaDTO.setDataFim(turma.getDataFim());
+		turmaDTO.setDataInicio(turma.getDataInicio());
+		turmaDTO.setDuracaoTurma(turma.getDuracaoTurma());
+		turmaDTO.setHorarioTurma(turma.getHorarioTurma());
+		turmaDTO.setIdTurma(turma.getIdTurma());
+		InstrutorDTO instrutorDTO = instrutorService.listarUmDTO(turma.getInstrutor().getIdInstrutor());
+		turmaDTO.setInstrutorDTO(instrutorDTO);
+		AtividadeDTO atividadeDTO = atividadeService.listarUmDTO(turma.getInstrutor().getIdInstrutor());
+		turmaDTO.setAtividadeDTO(atividadeDTO);
+		return turmaDTO;
+
 	}
-	/*public Boolean deleteTurmaComConferencia(Integer id) {
-		if(turmaRepository.findById(id).isPresent()) {
-			turmaRepository.deleteById(id);
-			return true;
-		}else {
-			return false;
-		}
-		
-	}*/
+	/*
+	 * public Boolean deleteTurmaComConferencia(Integer id) {
+	 * if(turmaRepository.findById(id).isPresent()) {
+	 * turmaRepository.deleteById(id); return true; }else { return false; }
+	 * 
+	 * }
+	 */
 }
