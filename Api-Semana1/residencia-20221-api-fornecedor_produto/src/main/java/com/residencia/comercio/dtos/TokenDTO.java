dbf75cccc0b98bf4e2d5ab51e89b1260c4877b3f
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-06-22 15:37:08 -0300

    Subindo alterações

---
+++ Api-Semana1/residencia-20221-api-fornecedor_produto/src/main/java/com/residencia/comercio/dtos/TokenDTO.java
@@ -0,0 +1,29 @@
+package com.residencia.comercio.dtos;
+
+public class TokenDTO {
+
+	private String type;
+	private String token;
+	
+	public TokenDTO(String type, String token) {
+		super();
+		this.type = type;
+		this.token = token;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+	
+	
+}
